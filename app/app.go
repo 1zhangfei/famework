a5420d9d41d3dc163f81cd7c3cb0f888abfc665b
dupengyi <17737913867@qq.com>
2024-03-08 09:41:14 +0800

    初始化

--- app/app.go
+++ app/app.go
@@ -1,1 +1,22 @@
 package app
+
+import (
+	"github.com/1zhangfei/famework/config"
+	"github.com/spf13/viper"
+)
+
+func Init(address string) error {
+
+	if err := config.ViperInit(address); err != nil {
+		return err
+	}
+	ip := viper.GetString("Nacos.Ip")
+	Port := viper.GetInt("Nacos.Port")
+
+	err := config.GetClient(ip, int64(Port))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
